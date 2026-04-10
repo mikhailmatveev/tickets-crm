c6cb6f5caa44c50ec689b6b59b0db97c834a1788
mu.matveev <mikhailmatveev.tmb@gmail.com>
2026-04-10 19:59:00 +0300

    Создание логики авторизации на бекенде

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class UserController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        return UserResource::collection(User::all());
+    }
+
+    public function show(Request $request): UserResource
+    {
+        return new UserResource($request->id);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,25 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Даёт возможность сохранять сессию для SPA
+        $middleware->statefulApi();
+        // Для API-запросов вывод только в JSON (редиректы не нужны)
+        $middleware->redirectGuestsTo(function (Request $request) {
+            return $request->is('api/*')
+                ? null
+                : route('login')
+            ;
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
