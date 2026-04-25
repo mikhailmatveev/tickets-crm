87d85ad308ead504f155abf58617402d3d493801
mu.matveev <mikhailmatveev.tmb@gmail.com>
2026-04-25 23:34:22 +0300

    Убрал ненужные комментарии

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -12,9 +12,6 @@ use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         Telescope::night();
