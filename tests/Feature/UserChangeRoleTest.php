e9bdb0f5d9f53e1e7ea0ed5f929feb7b7dfa6183
mu.matveev <mikhailmatveev.tmb@gmail.com>
2026-04-27 18:09:58 +0300

    Добавил тесты смены роли для PUT /api/user/{id}/role

---
+++ tests/Feature/UserChangeRoleTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Feature;
+
+use App\Enums\User\Role as RoleEnum;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Tests\Feature\UserTest;
+
+class UserChangeRoleTest extends UserTest
+{
+    protected array $userData = [
+        'email' => 'user@example.com'
+    ];
+
+    /**
+     * Тест смены роли пользователю из-под админа (ожидаем 200-й ответ)
+     * @return void
+     */
+    public function test_admin_can_change_role(): void
+    {
+        // Логинимся как админ
+        $this->actingAsAdmin();
+        // Тестовый пользователь
+        $user = User::factory()
+            ->create($this->userData)
+            ->assignRole(RoleEnum::ADMIN)
+        ;
+        // PUT /api/user/{id}/role
+        $response = $this->putJson("/api/user/{$user->id}/role", [
+            'role_id' => $this->getValidRoleId(RoleEnum::MANAGER)
+        ]);
+        // 200-й ответ
+        $response->assertOk();
+        // Найдём тестового пользователя в базе
+        $user = User::where('email', $this->userData['email'])
+            ->firstOrFail()
+        ;
+        // Проверяем, что роль у пользователя осталась прежней
+        $this->assertTrue($user->hasRole(RoleEnum::MANAGER));
+    }
+
+    /**
+     * Тест смены роли из-под менеджера (ожидаем 403-й ответ)
+     * @return void
+     */
+    public function test_manager_cannot_change_role(): void
+    {
+        // Логинимся как менеджер
+        $this->actingAsManager();
+        // Тестовый пользователь
+        $user = User::factory()
+            ->create($this->userData)
+            ->assignRole(RoleEnum::ADMIN)
+        ;
+        // PUT /api/user/{id}/role
+        $response = $this->putJson("/api/user/{$user->id}/role", [
+            'role_id' => $this->getValidRoleId(RoleEnum::MANAGER)
+        ]);
+        // 403-й ответ
+        $response->assertForbidden();
+        // Найдём тестового пользователя в базе
+        $user = User::where('email', $this->userData['email'])
+            ->firstOrFail()
+        ;
+        // Проверяем, что роль у пользователя осталась прежней
+        $this->assertTrue($user->hasRole(RoleEnum::ADMIN));
+    }
+
+    /**
+     * Хелпер-метод получения ID роли по её имени
+     * @param RoleEnum $role Роль
+     * @return int ID роли в таблице roles
+     */
+    protected function getValidRoleId(RoleEnum $role): int
+    {
+        return Role::where('name', $role)
+            ->first()
+            ->id
+        ;
+    }
+}
