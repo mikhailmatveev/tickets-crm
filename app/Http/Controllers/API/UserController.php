ffbe6882638a6e88eb935e1c8fb41024b1b0909d
mu.matveev <mikhailmatveev.tmb@gmail.com>
2026-06-18 21:40:11 +0300

    Метод возвращает конкретную коллекцию вместо анонимной

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,11 +7,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UserCreateRequest;
 use App\Http\Requests\UserDeleteRequest;
 use App\Http\Resources\UserResource;
+use App\Http\Resources\UserResourceCollection;
 use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class UserController extends Controller
 {
@@ -19,9 +18,9 @@ class UserController extends Controller
         protected UserService $userService
     ) {}
 
-    public function index(): AnonymousResourceCollection
+    public function index(): UserResourceCollection
     {
-        return UserResource::collection(
+        return UserResourceCollection::make(
             User::with('roles')
                 ->get()
         );
